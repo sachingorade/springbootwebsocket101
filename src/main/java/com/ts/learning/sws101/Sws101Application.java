391cf7fefc912dbaaa9e8f808f3c95981c41d780
Sachin Gorade <sachin.gorade@gmail.com>
2020-10-06 22:31:56 +0200

    First commit

---
+++ src/main/java/com/ts/learning/sws101/Sws101Application.java
@@ -0,0 +1,13 @@
+package com.ts.learning.sws101;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sws101Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sws101Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ts/learning/sws101/Sws101ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ts.learning.sws101;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sws101ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
